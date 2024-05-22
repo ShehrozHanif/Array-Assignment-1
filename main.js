da811fb4023f25570f23838735c9dc3c250f15f2
Shehroz Hanif <shehrozhanif54@gmail.com>
2024-05-22 07:00:45 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,40 @@
+//                           Array Assignment
+/* Challenge:
+ You are taskd with Creating a Inventory System.Implement functions and logic to manage object manupulation effectively.
+
+ 1.Define a type alias named product to represent a product with the following properties:
+ . name(string): The name of the product.
+ . price(number): The price of producr.
+ . inventory(Object): An object containing invemtory details including:
+ . stock(number):The number of products is available.
+ . colorOptions(string[]):An array listing available color
+*/
+/*2.Create an Array named products containing atleast three product objects.Each product object shuold include a name, price and inventory details.*/
+let products = [
+    {
+        name: "Louis Vuitton bag",
+        price: 15000,
+        inventory: {
+            stock: 100,
+            colorOptions: ["brown", "black"]
+        }
+    },
+    {
+        name: "Gucci bag",
+        price: 10000,
+        inventory: {
+            stock: 50,
+            colorOptions: ["brown", "red", "white"]
+        }
+    },
+    {
+        name: "Mk tote bag ",
+        price: 8000,
+        inventory: {
+            stock: 200,
+            colorOptions: ["multi color", "pink"]
+        }
+    }
+];
+export {};
+// Ramaining Question & Answers in main2.ts file 
---
+++ main2.ts
@@ -0,0 +1,148 @@
+/*3.Impelment a function name changeColor that takes a product object and a new color as input.This function shuld update the color property
+of the product and adjust the price based on the new color (implement your own logic, e.g increase by 10 % for red decrease 5% for blue).
+*/
+
+//Accoring to this question we add color property in type product
+  
+
+type product ={
+    name:string,
+    price:number,
+    color:string, 
+    inventory:{
+        stock:number,
+        colorOptions:string[]
+    }
+}
+
+// we place object in an Array
+
+let products:product[]=[
+    {
+        name:"Louis Vuitton bag",
+        price:15000,
+        color:"white",
+        inventory:{
+            stock:100,
+            colorOptions:["red","blue"]
+        }
+    },
+    {
+        name:"Gucci bag",
+        price:10000,
+        color:"black",
+        inventory:{
+            stock:50,
+            colorOptions:["red","blue","white"]
+        } 
+    },
+    {
+        name:"Mk tote bag ",
+        price:8000,
+        color:"Green",
+        inventory:{
+            stock:200,
+            colorOptions:["red","blue","pink"]
+        }
+    }
+]
+
+
+  
+function changeColor(products:product,newColor:string){
+    products.color = newColor
+    switch(newColor){
+        case "red":
+            products.price *=1.10;
+            break;
+        case "blue":
+            products.price *=0.95;
+            break;
+        default :
+            // No price change for other colors
+          break; 
+
+
+    }
+}
+
+// Now i explain what is [0] index, as we all know this index start from 0 and all these objects in an Array named products and his 3 object in 
+//Array like   products:string[] = ["Louis Vuitton","Gucci","M.k"],  So as you can see How it is so when we call products[0] it's mean we are  
+// calling "Louis Vuitton" , similarly for [1] index means we are calling "Gucci" object so in this part we learn how to add object in an Array
+// and when implemnting a function in an Object than how we can execute it.
+
+changeColor(products[2],'blue')
+console.log(products[2])
+
+
+
+/*4.Display each product's name,price,stock and available colors.Iterate through the products array and print each product's details. 
+  
+ For iteration in an Array we use of lopp
+*/
+
+for(let product of products){
+    console.log(`Name:${product.name}`);
+    console.log(`Price:${product.price}`);
+    console.log(`Stock:${product.inventory.stock}`);
+    console.log(`Available Colors:${product.inventory.colorOptions}`);
+
+    console.log('----')//it's a seprator 
+
+}
+ 
+
+
+
+/// Part 2: Multi-Dimentional Arrays and Tuples - student Grades
+// Challenge:
+//1.Define a typeScript type alias named student to represent a student with the following properties:
+//. name(string):The name of the student.
+//. grades(number[]):An array of grades for different subjects.
+
+
+type student={
+    name:string,
+    grades:number[]
+}
+
+// 2.Create an array named students containing atleast three student object.Each student Object should include a name and an array of grades.
+
+const students:student[]=[
+    {
+        name:"Shehroz",
+        grades:[80,85,90]
+    },
+    {
+        name:"Shami",
+        grades:[78,72,85]
+    },
+    {
+        name:"shayan",
+        grades:[85,70,68]
+    }
+]
+
+//3. Implement a function named CalculateAverageGrade that takes a student's grade as input and returns the average grade for that student.
+
+
+function CalculateAverageGrade(grade:number[]):number{
+    let result = grade.reduce((total,sum)=>sum+total,0)
+    return result / grade.length
+} 
+
+//4 . Display each student's name and average grade.Iterating through the students array.Calculate the average grade for each student using the
+//    calculateAverageGrade function and print their name and average grade.
+
+// Aagain using  of-lopp
+
+
+for(let student of students){
+    let averageGrades = CalculateAverageGrade(student.grades)
+    console.log(`Name:${student.name}`)
+    console.log(`Grades:${student.grades}`)
+    console.log(`AverageGrades:${averageGrades.toFixed(2)}`)
+    console.log('----') // As usual seprator
+}  
+
+
